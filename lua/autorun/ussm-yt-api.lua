d3df8e8390bc62865dda4d322b0ac26354f1b21f
aNgelOKK <71973953+AngoNex@users.noreply.github.com>
2025-05-09 03:27:48 +0300

    v 1.0.0

---
+++ lua/autorun/ussm-yt-api.lua
@@ -0,0 +1,107 @@
+if not SERVER then return end
+
+local JSONToTable = util.JSONToTable
+local SetGlobal2Var = SetGlobal2Var
+local string_match = string.match
+local string_find = string.find
+local CurTime = CurTime
+local HTTP = HTTP
+
+local function printf( fmt, ... )
+	return print( string.format( fmt, ... ) )
+end
+
+local api_adress = nil
+
+local function api_query( id )
+	HTTP( {
+		url = api_adress .. "/prepare-download/" .. id,
+		method = "GET",
+		success = function( code, body, headers )
+			local result = JSONToTable( body )
+            if result.ready then
+                ussm.SetStartTime( CurTime() )
+				SetGlobal2Var( "ussm-file-path", api_adress .. "/download/" .. result.id )
+			end
+		end,
+		failed = function( reason )
+			HTTP( {
+				url = api_adress .. "/status/" .. id,
+				method = "GET",
+				success = function( code, body, headers )
+					local result = JSONToTable( body )
+					local status = result.status
+					if status == "unknown" then
+                        printf( "[USSM-YT-API/Error] External api error, unknown request status" )
+					elseif status == "cached"  then
+                        ussm.SetStartTime( CurTime() )
+						SetGlobal2Var( "ussm-file-path", api_adress .. "/download/" .. result.id )
+					elseif status == "downloading" then
+						api_query( id )
+					else
+						printf( "[USSM-YT-API/Error] External api error, check api" )
+					end
+				end,
+				failed = function( reason )
+					printf( "[USSM-YT-API/Error] External api error, api is not responding" )
+				end
+			} )
+		end
+	} )
+end
+
+function ussm.SetFilePath( filePath )
+	if not filePath or filePath == "" or filePath == "none" or filePath == "nil" then
+		SetGlobal2Var( "ussm-start-time", nil )
+		SetGlobal2Var( "ussm-file-path", nil )
+		return
+	end
+
+	local domain = string_match( filePath, "^https?://([^/]+)" )
+    if domain then
+		domain = domain:lower()
+		local id = nil
+		if string_find( domain, "youtube%.com$" ) then
+			id = string_match( filePath, "[?&]v=([%w_-]+)" )
+		end
+
+		if string_find( domain, "youtu%.be$" ) then
+			id = string_match( filePath, "youtu%.be/([%w_-]+)" )
+		end
+
+		if id and api_adress then
+			api_query( id )
+		else
+			SetGlobal2Var( "ussm-start-time", CurTime() )
+			SetGlobal2Var( "ussm-file-path", filePath )
+		end
+	end
+
+end
+
+local function check_api( adress )
+    HTTP( {
+        url = adress .. "/status",
+        method = "GET",
+        success = function( code, body, headers )
+            local result = JSONToTable( body )
+            local status = result.status
+            if status then
+                api_adress = adress
+                printf( "[USSM-YT-API/Info] API connection established" )
+            else
+                printf( "[USSM-YT-API/Error] External api error, check api" )
+            end
+        end,
+        failed = function( reason )
+            printf( "[USSM-YT-API/Error] External api error, api is not responding" )
+        end
+    } )
+end
+
+local api_cvar = CreateConVar( "sv_ussm_api", "", bit.bor( FCVAR_ARCHIVE, FCVAR_PROTECTED ), "URL address of the deployed external api." )
+check_api( api_cvar:GetString() )
+
+cvars.AddChangeCallback( api_cvar:GetName(), function( _, __, value )
+	check_api( value )
+end )
